bf848b9291c8c9c5707b4f3bb1c76df29987d493
SuperMaZingCoder <supermazingcoder@gmail.com>
2020-12-14 04:24:23 -0500

    Fixed __main__.py, needs a PR for better code.

--- sotrace/__main__.py
+++ sotrace/__main__.py
@@ -1,17 +1,19 @@
 import argparse
 import importlib
 import os
+import subprocess
 
 from .sotrace import open_links
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="File to execute with sotrace.")
 parser.add_argument("--results", type=int, default=1, help="Number of results to open.")
-parser.add_argument("--tags", type=list, default=["python"], help="Preferenced tags.")
+parser.add_argument("--tags", default=["python"], help="Preferenced tags.", nargs="+", type=str)
 args = parser.parse_args()
 
+# Someone please make a pull request to make this code better
 try:
-    module = importlib.import_module(os.path.splitext(args.file)[0])
+    subprocess.call(f"python3 {args.file}", shell=True)
+    importlib.import_module(os.path.splitext(args.file)[0])
 except Exception as e:
-    print(type(e).__name__ + ": " + str(e))
     open_links(e, tags=args.tags, num_of_results=args.results)
